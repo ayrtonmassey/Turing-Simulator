a1026ae052803d1c3bc20c7d236da6f22b35119a
Ayrton Massey <ayrtonmassey@googlemail.com>
2012-04-05 19:40:41 +0100

    Updated documentation for Simulator class.

--- Turing-Simulator-Project/src/turing/interfaces/Simulator.java
+++ Turing-Simulator-Project/src/turing/interfaces/Simulator.java
@@ -20,14 +20,12 @@ public interface Simulator {
 
 	/**
 	 * The column index of the tape head is equivalent to its x-coordinate if you imagine the tape as a grid.
-	 * As our tape is currently one-dimensional, have this method return the current position of the tape head.
 	 * @return the column index of the tape head on the tape.
 	 */
 	public int getTapeHeadColumnIndex();
 	
 	/**
 	 * The row index of the tape head is equivalent to its y-coordinate if you imagine the tape as a grid.
-	 * As our tape is currently one-dimensional, have this method return 0.
 	 * @return the row index of the tape head on the tape.
 	 */
 	public int getTapeHeadRowIndex();
@@ -35,10 +33,8 @@ public interface Simulator {
 	/**
 	 * Sets the symbol on the tape at (<code>rowIndex</code>,<code>columnIndex</code>) to <code>symbol</code>.
 	 * <p>
-	 * Although our tape is currently one-dimensional, including <code>rowIndex</code> and <code>columnIndex</code>
-	 * will make it possible to extend this to a two-dimensional tape at a later time.
-	 * <p>
-	 * For now, take <code>columnIndex</code> as the index of the cell on the tape.
+	 * Because the tape is of infinite length, this method may try to edit cells which are not in the
+	 * tape data structure. This method should create those cells and then assign the value to them.
 	 * @param symbol The symbol to write to the tape.
 	 * @param rowIndex The row index of the cell to write to.
 	 * @param columnIndex The column index of the cell to write to.
@@ -48,15 +44,11 @@ public interface Simulator {
 	public boolean setTapeCellSymbol(char symbol,int rowIndex,int columnIndex) throws TuringException;
 	
 	/**
-	 * Returns the contents of the tape between beginIndex and endIndex inclusive.
-	 * <p>
-	 * The size of the returned list should be (beginIndex-endIndex)+1, as 0 is an index in the list.
+	 * Returns the contents of the tape between rowBeginIndex, rowEndIndex, colBeginIndex and colEndIndex.
 	 * <p>
 	 * As the tape is of infinite length, this method may request cells which do not exist
 	 * in the data structure which represents the tape. This method should return blanks
 	 * for any cells which do not exist.
-	 * <p>
-	 * For now, ignore the row parameters - these have been added to make way for a two-dimensional tape later.
 	 * @param rowBeginIndex The first row of the section of the tape to return.
 	 * @param rowEndIndex The last row of the section of the tape to return.
 	 * @param colBeginIndex The first column of the section of the tape to return.
@@ -67,12 +59,27 @@ public interface Simulator {
 	public List<List<Character>> getTapeContents(int rowBeginIndex,int rowEndIndex,int colBeginIndex,int colEndIndex);
 
 	/**
-	 * Returns the {@link Instruction} currently being executed by the Turing simulator.
 	 * @return the {@link Instruction} currently being executed by the Turing simulator.
 	 */
 	public Instruction getCurrentInstruction();
 
+	/**
+	 * Returns the original x-coordinate of the tape head.
+	 * <p>
+	 * The origin is the cell at position (0,0) - the initial position of the tape head.
+	 * Because the tape data structure can change, the origin cell may move to a different position.
+	 * To counter this, this method should return the current position of the origin cell.
+	 * @return The x-coordinate of the current position of the origin cell.
+	 */
 	public int getTapeOriginX();
 
+	/**
+	 * Returns the original y-coordinate of the tape head.
+	 * <p>
+	 * The origin is the cell at position (0,0) - the initial position of the tape head.
+	 * Because the tape data structure can change, the origin cell may move to a different position.
+	 * To counter this, this method should return the current position of the origin cell.
+	 * @return The y-coordinate of the current position of the origin cell.
+	 */
 	public int getTapeOriginY();
 }
