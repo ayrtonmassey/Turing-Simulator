6488347ec328c87164c8457c5efb48f93245203f
Ayrton Massey <ayrtonmassey@googlemail.com>
2012-04-24 20:00:25 +0100

    Commit before pull

--- Turing-Simulator-Project/src/turing/simulator/TuringInstruction.java
+++ Turing-Simulator-Project/src/turing/simulator/TuringInstruction.java
@@ -1,5 +1,6 @@
 package turing.simulator;
 
+import turing.TuringException;
 import turing.interfaces.Instruction;
 
 public class TuringInstruction implements Instruction {
@@ -19,6 +20,53 @@ public class TuringInstruction implements Instruction {
 		this.direction=direction;
 	}
 	
+	public TuringInstruction(String quintuplet) throws TuringException
+	{
+		if(quintuplet.matches(Instruction.QUINTUPLET_REGEX))
+		{
+			quintuplet = quintuplet.substring(1,quintuplet.length()-1);
+			
+			String[] instructionArray = quintuplet.split(",");
+			
+			int currentState = Integer.parseInt(instructionArray[0]);
+			char inputSymbol = instructionArray[1].charAt(0);
+			int nextState = Integer.parseInt(instructionArray[2]);
+			char outputSymbol = instructionArray[3].charAt(0);
+			char directionChar = instructionArray[4].charAt(0);
+			int direction;
+			switch(directionChar)
+			{
+			case 'U':
+				direction = Instruction.MOVE_UP;
+				break;
+			case 'D':
+				direction = Instruction.MOVE_DOWN;
+				break;
+			case 'L':
+				direction = Instruction.MOVE_LEFT;
+				break;
+			case 'R':
+				direction = Instruction.MOVE_RIGHT;
+				break;
+			case 'H':
+				direction = Instruction.HALT;
+				break;
+			default:
+				direction = -1;
+			}
+			
+			this.currentState=currentState;
+			this.inputSymbol=inputSymbol;
+			this.nextState=nextState;
+			this.outputSymbol=outputSymbol;
+			this.direction=direction;
+		}
+		else
+		{
+			throw new TuringException("Quintuplet "+quintuplet+" is not a valid instruction for this Turing machine simulator.");
+		}
+	}
+
 	@Override
 	public int getCurrentState()
 	{
